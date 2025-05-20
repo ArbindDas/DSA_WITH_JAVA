ab95910993e5f69c255b85c496e11b3e216426ea
Arbind Das <dasarbind269@gmail.com>
2025-05-20 14:02:16 +0545

    Done

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,28 @@
+package DSA.Sorting;
+
+import static DSA.Sorting.SelectionSort.printArray;
+
+public class BubbleSort {
+    public static void isBubbleSort(int []arr){
+        boolean isSwapped;
+        for (int i = 0 ; i < arr.length-1 ; i++) {
+            isSwapped = false;
+            for (int j = 0 ; j < arr.length-1-i ; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped){
+                break;
+            }
+        }
+        printArray (arr);
+    }
+    public static void main ( String[] args ) {
+       int []arr = new int[]{89 ,  34 , 1 , 4};
+        isBubbleSort(arr);
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,27 @@
+package DSA.Sorting;
+
+public class SelectionSort {
+    public static void printArray(int[]arr){
+        for (int value : arr) {
+            System.out.println (value);
+        }
+    }
+    public static void  isSelectionSort(int []arr){
+        for (int i = 0 ; i < arr.length ; i++) {
+                int minIndex = i;
+            for (int j = i+1 ; j <  arr.length; j++) {
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        printArray (arr);
+    }
+    public static void main ( String[] args ) {
+        int [] arr = new int[]{4,5,3,2,1};
+        isSelectionSort (arr);
+    }
+}
